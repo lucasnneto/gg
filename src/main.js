bf0f939d4064701aa51e7f59db40439e36d2ff2a
Lucas Neto <lucasn-oliveira@hotmail.com>
2022-01-04 17:23:33 -0300

    fix: dotenv

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import dotenv from "dotenv";
 import "animate.css";
 
+dotenv.config();
 Vue.config.productionTip = false;
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  configureWebpack: {
+    mode: process.env.NODE_ENV,
+  },
   transpileDependencies: ["vuetify"],
 };
