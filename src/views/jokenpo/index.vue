5949046d457111e18c49a471718eceb6f97541db
Lucas Neto <lucasn-oliveira@hotmail.com>
2022-01-05 08:37:42 -0300

    desabilitando jogo contra player

--- src/views/jokenpo/index.vue
+++ src/views/jokenpo/index.vue
@@ -3,7 +3,7 @@
     <h1>Pedra Papel Tesoura</h1>
     <router-link to="/jokenpo/com">Contra Maquina</router-link>
     <br />
-    <router-link to="/jokenpo/123">Contra Player</router-link>
+    <!-- <router-link to="/jokenpo/123">Contra Player</router-link> -->
     <br />
     <router-link to="/">voltar</router-link>
   </v-container>
